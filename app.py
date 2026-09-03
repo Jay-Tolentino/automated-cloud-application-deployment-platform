182d1f22135b8a4ec32feed4ce189b1d6c6064df
Jay Tolentino <jayt98123@gmail.com>
2026-09-03 16:12:14 -0700

    Test automated deployment pipeline

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Jay's Cloud App is Live</h1>
+    <h1>Jay's Automated Cloud App is Live</h1>
     <p>Running on AWS EC2 🚀</p>
     """
